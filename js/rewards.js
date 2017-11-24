c07f3c3b17adba7409e00bc1ae8e8240813c6bb4
bitsoko <bitsoko@bitsoko.io>
2017-11-24 20:27:17 +0300

    Update rewards.js

--- js/rewards.js
+++ js/rewards.js
@@ -527,7 +527,7 @@ function tradeManager(oid, action) {
 
                 if (action == 'buy') {
 
-                    $(".tradeOrderSubTitle").html('BUYING ' + parseFloat(allOrds[ix].amount).toFixed(allTokens[activeCoin].decimals) + ' ' + (activeCoin + sss).toUpperCase());
+                    $(".tradeOrderSubTitle").html('BUYING ' + parseFloat(allOrds[ix].amount).toFixed(allTokens[allOrds[ix].coin].decimals) + ' ' + (activeCoin + sss).toUpperCase());
                     $(".tradeOrderBody").html('Send ' + (parseFloat(allOrds[ix].amount) * parseFloat(allOrds[ix].rate)).toFixed(2) + ' ' +
                         baseCd.toUpperCase() + ' to ' + allOrds[ix].tranFrom.name.split(" ") + ' at phone number ' + allOrds[ix].tranFrom.phone +
                         ' then enter the transaction code below.');
@@ -535,7 +535,7 @@ function tradeManager(oid, action) {
 
                     $(".transStat").html('waiting for seller to confirm payment..');
                 } else if (action == 'sell') {
-                    $(".tradeOrderSubTitle").html('SELLING ' + parseFloat(allOrds[ix].amount).toFixed(allTokens[activeCoin].decimals) + ' ' + (activeCoin + sss).toUpperCase());
+                    $(".tradeOrderSubTitle").html('SELLING ' + parseFloat(allOrds[ix].amount).toFixed(allTokens[allOrds[ix].coin].decimals) + ' ' + (activeCoin + sss).toUpperCase());
                     $(".tradeOrderBody").html('Recieve ' + (parseFloat(allOrds[ix].amount) * parseFloat(allOrds[ix].rate)).toFixed(2) + ' ' +
                         baseCd.toUpperCase() + ' at phone number ' + allOrds[ix].tranTo.phone +
                         ' then enter the transaction code below.');
@@ -658,8 +658,7 @@ $('.preloader-wrapper').css('display','none');
                     }
 			
 			var deci =allTokens[oDs[ii].coin] ? allTokens[oDs[ii].coin].decimals : 0;
-			console.log(deci);
-
+			
                     if (parseInt(oDs[ii].tranTo.uid) == parseInt(localStorage.getItem('bits-user-name'))) {
 
                         $("#myOrders").append('<tr class="element-' + oDs[ii].coin + '-coin element-all-coin">' +
@@ -708,7 +707,7 @@ $('.preloader-wrapper').css('display','none');
 
                         $("#orderbookSep").before('<tr class="element-' + oDs[ii].coin + '-coin element-all-coin" style="background-color:#ffdcdc;height: 40px;" >' +
                             '<td ><img src="' + oDs[ii].tranFrom.icon + '" style="width: 35px;float: left;border-radius: 50px;position: relative;left: 10px;"><span class="odbk-txt hide-on-med-and-down">' + oDs[ii].tranFrom.name + '</span></td>' +
-                            '<td class="hidden-xs">' + parseFloat(oDs[ii].amount).toFixed(5) + '</td>' +
+                            '<td class="hidden-xs">' + parseFloat(oDs[ii].amount).toFixed(deci) + '</td>' +
                             '<td class="hidden-xs">' + parseFloat(oDs[ii].rate).toFixed(5) + '</td>' +
                             '<td>' + (parseFloat(oDs[ii].amount) * parseFloat(oDs[ii].rate)).toFixed(2) + '</td>' +
                             '<td>' + bAc +
@@ -750,7 +749,7 @@ $('.preloader-wrapper').css('display','none');
 
                         $("#orderbookSep").after('<tr class="element-' + oDs[ii].coin + '-coin element-all-coin" style="background-color:#dcffdc;height: 40px;">' +
                             '<td ><img src="' + oDs[ii].tranTo.icon + '" style="width: 35px;float: left;border-radius: 50px;position: relative;left: 10px;"><span class="odbk-txt hide-on-med-and-down">' + oDs[ii].tranTo.name + '</span></td>' +
-                            '<td class="hidden-xs">' + parseFloat(oDs[ii].amount).toFixed(5) + '</td>' +
+                            '<td class="hidden-xs">' + parseFloat(oDs[ii].amount).toFixed(deci) + '</td>' +
                             '<td class="hidden-xs">' + parseFloat(oDs[ii].rate).toFixed(5) + '</td>' +
                             '<td>' + (parseFloat(oDs[ii].amount) * parseFloat(oDs[ii].rate)).toFixed(2) + '</td>' +
                             '<td>' + bAc +
