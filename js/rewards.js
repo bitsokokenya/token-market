6ee37ecd43760947d25e36075890cb04786b1411
bitsoko <bitsoko@bitsoko.io>
2018-05-31 08:50:37 +0300

    Update rewards.js

--- js/rewards.js
+++ js/rewards.js
@@ -1441,7 +1441,7 @@ function orderBookManager(baseX, baseCd) {
                                 //this is a buy order
                                 oDs[ii].type = 'buy';
                                      
-                                if((allTokens['eth'].balance / Math.pow(10, allTokens['eth'].decimals)*baseX*baseConv)>(parseFloat(oDs[ii].amount) * parseFloat(oDs[ii].rate)).toFixed(2)){
+                                if((allTokens['eth'].balance / Math.pow(10, allTokens['eth'].decimals)*baseX*baseConv)>(parseFloat(oDs[ii].amount) * parseFloat(oDs[ii].rate)).toFixed(2)  || activeCoin.toLowerCase()=='eth'){
                                        var bAc = '<a id="oid-act-' + oDs[ii].id + '" class=" waves-effect waves-light btn modal-trigger" href="#tradeOrder" oid="' + oDs[ii].id + '" act="buy">BUY</a>';
                                 
                                    }else {
