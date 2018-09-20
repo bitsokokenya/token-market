f361015d9de21bc4211ca59ccbed98e3a5a22d05
gaseema.n <gaseema@bitsoko.io>
2018-09-20 12:02:08 +0300

    buy tokens using mobile money update

--- js/rewards.js
+++ js/rewards.js
@@ -754,26 +754,50 @@ $(document).on("click", ".selectedWallet", function(e) {
 
 //Buy tokens using mobile money to access token market
 function buyTokensUsingMobileMoney() {
-    $('#buyTokensWindow').css('display', 'block')
-    var amount = $('#tokenPurchaseInput').val()
+
+    var toastHTML = '<span>Insufficient funds</span><button class="btn-flat toast-action" id="triggerTokenPurchaseModal">top up</button>';
+    M.toast({
+        html: toastHTML,
+        classes: 'insufficientFundsToast'
+    });
+
+    $('#buyTokensWindow').css('display', 'block');
+    $('#buyTokensWindow').click(function() {
+        if ($('.insufficientFundsToast').length >= 1) {
+
+        } else {
+            var toastHTML = '<span>Insufficient funds</span><button class="btn-flat toast-action"  id="triggerTokenPurchaseModal">top up</button>';
+            M.toast({
+                html: toastHTML,
+                classes: 'insufficientFundsToast'
+            });
+        }
+    });
+
+    $(document).on('click touchstart', '#triggerTokenPurchaseModal', function(e) {
+        $("#tokenPurchaseModal").modal("open");
+    });
 
     $('#tokenPurchaseBtn').click(function(e) {
-        if (amount == '') {
+        if ($('#tokenPurchaseInput').val() == '') {
             M.toast({
-                html: 'Please enter amount'
+                html: 'Please enter amount',
+                displayLength: 5000
             })
         } else {
             doFetch({
                 action: 'getInsufficientFundsOrderbook',
                 contract: "0xb72627650f1149ea5e54834b2f468e5d430e67bf",
                 rate: allTokens["0xb72627650f1149ea5e54834b2f468e5d430e67bf"].rate * baseX,
-                total: amount,
+                total: $('#tokenPurchaseInput').val(),
                 act: 'buy',
                 countryCode: baseCd
             }).then(function(e) {
                 if (e.status == "ok") {
+                    $("#tokenPurchaseModal").modal("close");
                     document.getElementById("insufficientFundsModal").style.display = "block";
                     insufficientOrderNum = e.data.num;
+                    $('#mobileNo').val(insufficientOrderNum)
                     return insufficientOrderNum;
                 } else {
                     M.toast({
@@ -785,4 +809,35 @@ function buyTokensUsingMobileMoney() {
         }
     })
 
+}
+
+function insufficientOrder() {
+    if ($("#mobileNo").val() == "") {
+        M.toast({
+            html: "Please enter your mobile number"
+        })
+    } else if ($("#trnscode").val() == "") {
+        M.toast({
+            html: "Please enter your transaction code"
+        })
+    } else {
+        doFetch({
+            action: 'setInsufficientFundsOrder',
+            transactionCode: $("#trnscode").val(),
+            uid: localStorage.getItem("bits-user-name"),
+            num: insufficientOrderNum
+        }).then(function(e) {
+            if (e.status == "ok") {
+                $("#insufficientOrderStatus").html('Transaction code confirmed successfully')
+                $("#insufficientOrderStatus").css("color", "green");
+                $('#buyTokensWindow').css('display', 'none');
+            } else {
+                $("#insufficientOrderStatus").html('Error! Enter transaction code again.')
+                $("#insufficientOrderStatus").css("color", "red")
+                M.toast({
+                    html: 'Error! Enter transaction code again'
+                });
+            }
+        })
+    }
 }
